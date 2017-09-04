2787f28f1bee8629c5d55f2bf2c242b6d319b356
Ekaitz Segurola <sockand@gmail.com>
2017-09-04 23:44:23 +0200

    Add files via upload
    Adds project files.

---
+++ Java_Swing_Test/src/java_swing_test/GUI/RightPanelButton.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_swing_test.GUI;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author sockand
+ */
+public class RightPanelButton extends JButton{
+    
+    public RightPanelButton(JFrame jf){
+        //jf.add(this);
+    }
+    
+    
+}
